a086202acad6b8434ec97f4b37d87a3f3b87dad8
Axoraco <team.axoraco@gmail.com>
2025-12-25 13:03:25 +0530

    fix: add Unsplash to image remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,12 @@ const nextConfig: NextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
     imageSizes: [16, 32, 48, 64, 96, 128, 256],
     minimumCacheTTL: 60 * 60 * 24 * 30, // 30 days
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
   },
 
   // Performance optimizations
